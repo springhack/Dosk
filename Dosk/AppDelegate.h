5287a17d6823807aadd1fa43c02010be8074957d
springhack <springhack@live.cn>
2017-07-30 21:22:55 +0800

    Initial Commit

---
+++ Dosk/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Dosk
+//
+//  Created by SpringHack on 2017/7/30.
+//  Copyright © 2017年 Dosk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ Dosk/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  Dosk
+//
+//  Created by SpringHack on 2017/7/30.
+//  Copyright © 2017年 Dosk. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
